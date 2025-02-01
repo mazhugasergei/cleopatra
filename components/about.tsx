74a9e6d9d696a67b5daaa8813c8b276e30fc7d11
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-01 17:59:51 +1000

    update about text & add photo

--- components/about.tsx
+++ components/about.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/helpers/tailwind"
+import about from "@/public/about.jpg"
+import Image from "next/image"
 
 export interface AboutProps extends React.HTMLAttributes<HTMLDivElement> {
 	ref?: React.Ref<HTMLDivElement>
@@ -6,19 +8,30 @@ export interface AboutProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const About = ({ className, ...props }: AboutProps) => {
 	return (
-		<section id="about" className={cn("space-y-4", className)} {...props}>
-			<h2 className="border-b pb-4 text-4xl font-bold">About us</h2>
-			<p>
-				At Cleopatra Traiding Co., we specialize in sourcing, dismantling, and delivering vehicles and spare parts
-				worldwide. Based in Incheon, South Korea, we provide a seamless experience for customers looking for Korean,
-				German, American, and other top-brand vehicles. Our team ensures disassembly, secure container loading, and
-				efficient global shipping.
-			</p>
-			<p>
-				With multilingual support in Korean, English, Arabic, and Russian, we are committed to offering reliable and
-				professional service to clients worldwide. Whether you need a fully assembled car or specific parts, we
-				guarantee quality and satisfaction every step of the way.
-			</p>
+		<section id="about" className={cn("grid grid-cols-1 gap-4 md:grid-cols-[7fr_10fr] md:gap-8", className)} {...props}>
+			<div className="relative">
+				<Image
+					src={about}
+					alt=""
+					width={1000}
+					height={1000}
+					placeholder="blur"
+					className="absolute inset-0 h-full w-full rounded-xl object-cover object-[50%_20%]"
+				/>
+			</div>
+			<div className="space-y-4 py-4">
+				<h2 className="border-b pb-4 text-4xl font-bold">About us</h2>
+				<p>
+					At Cleopatra Trading Co., we specialize in sourcing and delivering vehicles and spare parts worldwide. Based
+					in Incheon, South Korea, we provide a seamless experience for customers looking for Korean, German, American,
+					and other top-brand vehicles. Our team ensures secure container loading and efficient global shipping.
+				</p>
+				<p>
+					With multilingual support in Korean, English, Arabic, and Russian, we are committed to offering reliable and
+					professional service to clients worldwide. Whether you need a fully assembled car or specific parts, we
+					guarantee quality and satisfaction every step of the way.
+				</p>
+			</div>
 		</section>
 	)
 }
