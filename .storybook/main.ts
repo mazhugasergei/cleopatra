5062d866c3f168f7f041c618c38ec80f772306eb
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-01 16:54:16 +1000

    storybook: clean starter addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
 	stories: ["../components/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: [
-		"@storybook/addon-onboarding",
-		"@storybook/addon-essentials",
-		"@chromatic-com/storybook",
-		"@storybook/addon-interactions",
-	],
+	addons: ["@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions"],
 	framework: {
 		name: "@storybook/nextjs",
 		options: {},
