31bf3524d53704b9a2dc98acfa3a53e49a674c3d
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-01 18:26:33 +1000

    header: add mobile menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,10 @@
+"use client"
+
+import { CloseIcon, MenuIcon } from "@/app/icons"
 import { cn } from "@/helpers/tailwind"
 import Link from "next/link"
-import { buttonVariants } from "./button"
+import React from "react"
+import { Button, buttonVariants } from "./button"
 import { Logo } from "./logo"
 
 export interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -11,12 +15,13 @@ export const Header = ({ className, children, ref, ...props }: HeaderProps) => (
 	<header
 		ref={ref}
 		className={cn(
-			"bg-background relative sticky top-0 z-50 flex items-center justify-between border-b py-2 pr-2",
+			"bg-background relative sticky top-0 z-50 flex items-center justify-between border-b py-2",
 			className
 		)}
 		{...props}
 	>
-		<Nav />
+		<Nav className="max-md:hidden" />
+		<Menu className="md:hidden" />
 		<Logo className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
 		<Link href="#contact" className={buttonVariants({ size: "lg", className: "rounded-none" })}>
 			Contact
@@ -24,25 +29,29 @@ export const Header = ({ className, children, ref, ...props }: HeaderProps) => (
 	</header>
 )
 
-export const Nav = () => {
-	const routes = [
-		{
-			href: "#",
-			label: "Home",
-		},
-		{
-			href: "#about",
-			label: "About",
-		},
-		{
-			href: "#services",
-			label: "Services",
-		},
-	]
+export const routes = [
+	{
+		href: "#",
+		label: "Home",
+	},
+	{
+		href: "#about",
+		label: "About",
+	},
+	{
+		href: "#services",
+		label: "Services",
+	},
+]
 
+export interface NavProps extends React.HTMLAttributes<HTMLDivElement> {
+	ref?: React.Ref<HTMLDivElement>
+}
+
+export const Nav = (props: NavProps) => {
 	return (
-		<nav>
-			<ul className="items-centera flex">
+		<nav {...props}>
+			<ul className="flex items-center">
 				{routes.map((route) => (
 					<li key={route.href}>
 						<Link href={route.href} className={buttonVariants({ variant: "link" })}>
@@ -52,5 +61,45 @@ export const Nav = () => {
 				))}
 			</ul>
 		</nav>
+	)
+}
+
+export interface MobileNavProps extends React.HTMLAttributes<HTMLDivElement> {
+	ref?: React.Ref<HTMLDivElement>
+}
+
+export const Menu = ({ className, ...props }: MobileNavProps) => {
+	const [open, setOpen] = React.useState(false)
+
+	React.useEffect(() => {
+		const close = () => setOpen(false)
+
+		window.addEventListener("scroll", close)
+		window.addEventListener("touchstart", close)
+
+		return () => {
+			window.removeEventListener("scroll", close)
+			window.removeEventListener("touchstart", close)
+		}
+	}, [])
+
+	return (
+		<div className={cn("relative", className)} {...props}>
+			<Button variant="ghost" size="icon" onClick={() => setOpen((prev) => !prev)} className={cn(open && "bg-accent")}>
+				{open ? <CloseIcon /> : <MenuIcon />}
+			</Button>
+
+			{open && (
+				<ul className="bg-background absolute left-0 top-[110%] rounded-md border p-2">
+					{routes.map((route) => (
+						<li key={route.href}>
+							<Link href={route.href} className={buttonVariants({ variant: "link" })}>
+								{route.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
 	)
 }
