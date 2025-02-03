eac39ee86c50dd5b6e1d825745f3d6696d7f3582
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-03 18:47:27 +1000

    icons: add size prop

--- components/contact.tsx
+++ components/contact.tsx
@@ -15,13 +15,7 @@ export const Contact = ({ className, ...props }: ContactProps) => {
 				<Contacts className="my-4 border-y py-4" />
 				<Socials />
 			</div>
-			<iframe
-				className="bg-secondary h-full w-full rounded-xl border"
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.6933385009274!2d126.6016822!3d37.5622882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b80225befd73d%3A0xe2ddd400940a884!2s1550%20Oryu-dong%2C%20Seo-gu%2C%20Incheon%2C%20South%20Korea!5e0!3m2!1sen!2sru!4v1738319140008!5m2!1sen!2sru"
-				allowFullScreen
-				loading="lazy"
-				referrerPolicy="no-referrer-when-downgrade"
-			/>
+			<Map />
 		</section>
 	)
 }
@@ -74,7 +68,7 @@ const socials = [
 	{
 		label: "Instagram",
 		href: "https://instagram.com/cleopatra_trd",
-		icon: <InstagramIcon />,
+		icon: <InstagramIcon size={20} />,
 	},
 ]
 
@@ -97,4 +91,19 @@ export const Socials = (props: SocialsProps) => (
 			</li>
 		))}
 	</ul>
+)
+
+export interface MapProps extends React.HTMLAttributes<HTMLIFrameElement> {
+	ref?: React.Ref<HTMLIFrameElement>
+}
+
+export const Map = ({ className, ...props }: MapProps) => (
+	<iframe
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.6933385009274!2d126.6016822!3d37.5622882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b80225befd73d%3A0xe2ddd400940a884!2s1550%20Oryu-dong%2C%20Seo-gu%2C%20Incheon%2C%20South%20Korea!5e0!3m2!1sen!2sru!4v1738319140008!5m2!1sen!2sru"
+		allowFullScreen
+		loading="lazy"
+		referrerPolicy="no-referrer-when-downgrade"
+		className={cn("bg-secondary h-full w-full rounded-xl border max-md:aspect-video", className)}
+		{...props}
+	/>
 )
