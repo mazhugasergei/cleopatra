ec3a4113f47557bf2dff2c91d27c9815b8ec2f73
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-04 00:29:10 +1000

    fonts: use local instead of online fonts

--- app/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Montserrat } from "next/font/google"
-
-export const headingFont = Montserrat({
-	weight: ["700"],
-	subsets: ["latin"],
-})
---
+++ app/fonts/index.ts
@@ -0,0 +1,13 @@
+// import { Montserrat } from "next/font/google"
+
+// export const headingFont = Montserrat({
+// 	weight: ["700"],
+// 	subsets: ["latin"],
+// })
+
+import localFont from "next/font/local"
+
+export const headingFont = localFont({
+	src: "./fonts/Montserrat-Bold.ttf",
+	display: "swap",
+})
