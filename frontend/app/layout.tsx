a0587c29a02c316a47993c7578bd207a88856ef3
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-07 18:18:19 +1000

    frontend: update title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-	title: "Cleopatra Trading Co. – Cars & Spare Parts Delivered Worldwide",
+	title: "Cleopatra Trading Co. | Cars & Spare Parts Delivered Worldwide",
 	description:
 		"Cleopatra Trading Co. specializes in sourcing, dismantling, and shipping high-quality cars and spare parts worldwide. Based in South Korea, we offer expert vehicle selection, professional disassembly, and secure global shipping. Trusted by customers worldwide for reliable service and multilingual support.",
 }
