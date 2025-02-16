d6a29102229b6adc60546aae97e1a256ab312e34
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-16 14:01:33 +1000

    frontend: add storybook sample intro page

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,8 +1,8 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-	stories: ["../components/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: ["@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions"],
+	stories: ["./**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
 	framework: {
 		name: "@storybook/nextjs",
 		options: {},
