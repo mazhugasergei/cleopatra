96401272a3bf7fefae634e116d583144fb24314a
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-24 12:20:48 +1000

    frontend: fix contact section height

--- frontend/components/sections/contact.tsx
+++ frontend/components/sections/contact.tsx
@@ -17,15 +17,7 @@ export const Contact = ({ dict, routes, className, ...props }: ContactProps) => 
 	const headerHeight = useHeaderHeight()
 
 	return (
-		<section
-			id="contact"
-			data-test="contact"
-			className={cn("flex flex-col", className)}
-			style={{
-				minHeight: `calc(100vh - ${headerHeight}px)`,
-			}}
-			{...props}
-		>
+		<section id="contact" data-test="contact" className={cn("flex h-screen flex-col", className)} {...props}>
 			<SectionHeader backLink="#services" title="Contact" routes={routes} />
 			<div className="wrapper grid flex-1 grid-cols-1 gap-4 py-4 md:grid-cols-[7fr_10fr]">
 				<div className="flex flex-col justify-center">
