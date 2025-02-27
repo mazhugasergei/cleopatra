362808eb56d460976bff8d4d29d1c8e00a477ba7
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-27 11:09:58 +1000

    Revert "frontend: fix routes" (only routes links)
    This reverts commit eb526fbc80064acdb99da6230749b42ec26e7aac.

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -25,19 +25,19 @@ export default async function RootLayout({ children, params }: LayoutProps) {
 
 	const routes = [
 		{
-			href: "/#hero",
+			href: "#hero",
 			label: dict?.header?.home,
 		},
 		{
-			href: "/#about",
+			href: "#about",
 			label: dict?.header?.about,
 		},
 		{
-			href: "/#services",
+			href: "#services",
 			label: dict?.header?.services,
 		},
 		{
-			href: "/#contact",
+			href: "#contact",
 			label: dict?.header?.contact,
 		},
 	]
